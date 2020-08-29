a5bc288f1b39cb04c361d624929e305c794d5e76
Luis Urán <luisuran.bit@gmail.com>
2020-08-29 12:15:41 -0300

    Agrego calculos para los dias

--- Time calculator.py
+++ Time calculator.py
@@ -8,6 +8,9 @@ def add_time(inicio, duracion, dia=None):
     hora_fin = h1 + h2
     minutos_fin = m1 + m2
 
+    dias = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+    canti_dias = 0
+
     while minutos_fin >=60:
         minutos_fin -= 60
         hora_fin += 1
@@ -18,13 +21,26 @@ def add_time(inicio, duracion, dia=None):
             periodo = 'PM'
         else:
             periodo = 'AM'
+            canti_dias += 1
 
-    if dia is None:
-        print("{}:{:02d} {}".format(hora_fin, minutos_fin, periodo))
-    else:
-        print("{}:{:02d} {}, {}".format(hora_fin, minutos_fin, periodo, dia.capitalize()))
+    if hora_fin == 0: hora_fin = 12
 
+    respuesta = "{}:{:02d} {}".format(hora_fin, minutos_fin, periodo)
+
+    if dia is not None:
+        pos = dias.index(dia.capitalize()) + canti_dias
+        if pos > 6:
+            pos = pos % 6
+        respuesta += ", {}".format(dias[pos])
+
+    if canti_dias == 1:
+        respuesta += " (next day)"
+
+    if canti_dias > 1:
+        respuesta += " ({} days later)".format(canti_dias)
     
+    print(respuesta)
+
 add_time("3:00 PM", "3:10")
 # Returns: 6:10 PM
  
@@ -33,3 +49,12 @@ add_time("11:30 AM", "2:32", "Monday")
  
 add_time("11:43 AM", "00:20")
 # Returns: 12:03 PM
+ 
+add_time("10:10 PM", "3:30")
+# Returns: 1:40 AM (next day)
+ 
+add_time("11:43 PM", "24:20", "tueSday")
+# Returns: 12:03 AM, Thursday (2 days later)
+ 
+add_time("6:30 PM", "205:12")
+# Returns: 7:42 AM (9 days later)
