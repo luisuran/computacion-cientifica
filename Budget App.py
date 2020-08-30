6c10ebad4dbf26580c4cd653766074db290ac511
Luis Urán <luisuran.bit@gmail.com>
2020-08-30 13:44:23 -0300

    Valido funciones de la clase Category

--- Budget App.py
+++ Budget App.py
@@ -1,9 +1,7 @@
 class Category:
-    nombre = ''
-    ledger = []
-
     def __init__ (self, nom):
         self.nombre = nom
+        self.ledger = []
 
     def deposit(self, monto, descripcion=''):
         self.ledger.append({"amount": monto, "description": descripcion})
@@ -11,7 +9,7 @@ class Category:
     def withdraw(self, monto, descripcion=''):
         if self.check_funds(monto):
             self.ledger.append({"amount": monto * -1, "description": descripcion})
-        return self.check_funds()
+        return self.check_funds(monto)
 
     def get_balance(self):
         balance = 0
@@ -22,7 +20,26 @@ class Category:
         return balance
 
     def transfer(self, monto, categoria):
-        pass
+        if self.check_funds(monto):
+            self.withdraw(monto, descripcion="Transfer to {}".format(categoria.nombre))
+            categoria.deposit(monto, descripcion="Transfer from {}".format(self.nombre))
+        return self.check_funds(monto)
 
     def check_funds(self, monto):
-        return monto > self.get_balance()
+        return self.get_balance() >= monto
+
+    def __str__(self):
+        resp = self.nombre.center(30, '*') + '\n'
+
+        for transaccion in self.ledger:
+            resp += transaccion['description'][:23]
+            canti = "{:.2f}".format(transaccion['amount'])  # Formateo el monto para que tenga 2 decimales
+            longi = len(transaccion['description'][:23])    # Calculo el espacio que ocupa el monto
+            resp += canti.rjust(30-longi, ' ') + '\n'       
+        
+        resp += "Total: {}".format(self.get_balance())
+
+        return resp
+
+def create_spend_chart():
+    pass
