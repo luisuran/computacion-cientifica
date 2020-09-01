f6fe3f04eca288ca0abc261a4a1858e5e815b92b
Luis Urán <luisuran.bit@gmail.com>
2020-09-01 16:16:30 -0300

    Agrego pruebas

--- Budget App.py
+++ Budget App.py
@@ -88,3 +88,22 @@ def create_spend_chart(categorias):
         resp += linea + "\n"
 
     return resp[:-1]    # Quito el último '\n'
+
+
+food = Category("Food")
+food.deposit(1000, "initial deposit")
+food.withdraw(10.15, "groceries")
+food.withdraw(15.89, "restaurant and more food for dessert")
+print(food.get_balance())
+clothing = Category("Clothing")
+food.transfer(50, clothing)
+clothing.withdraw(25.55)
+clothing.withdraw(100)
+auto = Category("Auto")
+auto.deposit(1000, "initial deposit")
+auto.withdraw(15)
+
+print(food)
+print(clothing)
+
+print(create_spend_chart([food, clothing, auto]))
