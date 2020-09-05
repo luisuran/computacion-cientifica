749f318c62bdd52f3782882223f784b165c333d7
Luis Urán <luisuran.bit@gmail.com>
2020-09-05 12:54:49 -0300

    Agrego funcion experiment

--- Probability Calculator.py
+++ Probability Calculator.py
@@ -1,3 +1,4 @@
+import copy
 import random
 
 class Hat:
@@ -18,4 +19,26 @@ class Hat:
         return bolas_sacadas
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
-    pass
+    hat_copia = copy.deepcopy(hat)
+    M = 0
+    expected = []
+    
+    for k, v in expected_balls.items():
+        for _ in range(v):
+            expected.append(k)
+
+    for _ in range(num_experiments):
+        hat_copia.contents = copy.deepcopy(hat.contents)
+        
+        sorteadas = hat_copia.draw(num_balls_drawn)
+
+        # Si encuentro una bola esperada en la lista de bolas sorteadas, la elimino de la lista de sorteadas
+        for x in expected:
+            if x in sorteadas:
+                sorteadas.remove(x)
+
+        # Si la cantidad de bolas en la lista sorteada coincide con la cantidad sacada menos la esperada, quiere 
+        # decir que obtuve todas las bolas
+        if len(sorteadas) == num_balls_drawn - len(expected): M += 1
+    
+    return M / num_experiments
